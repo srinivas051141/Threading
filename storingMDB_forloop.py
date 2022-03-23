69cf85a54865cc50a0d985d996c21c15152c68cf
srinivas051141 <tsaisrinivas999@gmail.com>
2022-03-24 00:17:00 +0530

    v2

---
+++ storingMDB_forloop.py
@@ -0,0 +1,39 @@
+import time
+import requests
+import pymongo
+import gridfs
+
+a=time.perf_counter()
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+database = client['Images']
+fs = gridfs.GridFS(database,collection='For loop')
+
+
+
+
+with open('links1.txt','r') as links:
+    urls = links.read()
+    urls = urls.split('\n')
+        
+    
+for i in range(len(urls[1:2])):
+     image_bytes =requests.get(urls[i]).content
+     #print((image_bytes))
+     img_name =f'{i}.jpg'
+     
+
+     fs.put(image_bytes, filename ='img_name')
+     
+
+b=time.perf_counter()
+print(f'{b-a} seconds')
+file = fs.find_one({'filename': 'img_name'})
+image = file.read()
+img_name ='1.jpg'
+with open(img_name, 'wb') as img_file:
+        #image_bytes =requests.get(urls[i]).content
+        img_file.write(image)
+        print(f'{img_name} was downloaded...')
+
