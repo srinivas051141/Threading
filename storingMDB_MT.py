27b1452e5ecfda6e471a6c27cac83af908230141
srinivas051141 <tsaisrinivas999@gmail.com>
2022-03-24 22:42:49 +0530

    v1

---
+++ storingMDB_MT.py
@@ -0,0 +1,35 @@
+import time
+import requests
+import concurrent.futures
+import pymongo
+import gridfs
+
+a=time.perf_counter()
+
+client =pymongo.MongoClient("mongodb://localhost:27017/")
+
+database = client['Images']
+fs = gridfs.GridFS(database,collection='Multi_Threading')
+
+with open('links1.txt','r') as links:
+    urls = links.read()
+    urls = urls.split('\n')
+    
+    
+   
+    
+   
+    
+#for i in range(len(urls[:1000])):
+def MT(url,i):
+     image_bytes =requests.get(url).content
+     #print((image_bytes))
+     img_name =f'{i}.jpg'
+     fs.put(image_bytes, filename ='img_name')
+     print(f'{img_name} is uploaded.')
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(MT,urls[:100],range(100))
+
+b=time.perf_counter()
+print(f'{b-a} seconds')
