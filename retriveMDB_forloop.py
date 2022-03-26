0033c8911f52919278dd4b15aa6c42b2cee59fac
srinivas051141 <tsaisrinivas999@gmail.com>
2022-03-26 17:57:38 +0530

    v2

--- retriveMDB_forloop.py
+++ retriveMDB_forloop.py
@@ -1,8 +1,9 @@
 import pymongo
 import time
 import gridfs
+from PIL import Image, ImageFilter
 
-
+size= (1200,1200)
 a = time.perf_counter()
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -14,7 +15,7 @@ with open('links1.txt','r') as links:
     urls = urls.split('\n')
 
 
-for i in range(len(urls[:2])):
+for i in range(len(urls[:1000])):
     img_name =f'{i}.jpg'
     file = fs.find_one({'filename' : img_name})
     image = file.read()
@@ -22,8 +23,14 @@ for i in range(len(urls[:2])):
          #image_bytes =requests.get(urls[i]).content
          img_file.write(image)
          print(f'{img_name} was downloaded...')
+         
+    img = Image.open(img_name)
+    img = img.filter(ImageFilter.GaussianBlur(15))
 
-#file = fs.find_one({'filename': 'img_name'})
-# image = file.read()
-# 
-# 
+    img.thumbnail(size)
+    img.save(f'{img_name}')
+    print(f'{img_name} was processed...')
+
+
+b= time.perf_counter()
+print(f'{b-a} seconds')
